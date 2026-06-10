11ac72feb3080dbfe5c98f30b775128ee3f20515
Anjali470 <anjalikonkala001@gmail.com>
2026-06-10 08:27:41 +0530

    Made migrations and Login functionalty

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('home', views.home, name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
-        user = User.object.create_user(username=username, password=password, email=email)
+        user = User.objects.create_user(username=username, password=password, email=email)
         user.save()
+        return redirect('login')
     return render(request, 'blog/signup.html')
 
 def login(request):
-    
-
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
     return render(request, 'blog/login.html')
+
+def home(request):
+    return render(request, 'blog/home.html')
