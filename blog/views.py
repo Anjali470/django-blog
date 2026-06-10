df6b745036a49a25b9872cd61d375e624a4b82a4
Anjali470 <anjalikonkala001@gmail.com>
2026-06-10 08:08:08 +0530

    Signup functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        user = User.object.create_user(username=username, password=password, email=email)
+        user.save()
     return render(request, 'blog/signup.html')
 
 def login(request):
+    
+
     return render(request, 'blog/login.html')
